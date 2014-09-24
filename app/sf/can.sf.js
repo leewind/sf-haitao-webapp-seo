287cce852480e9156d6399ad0a3638525fc6d069
Michael.Lee <leewind19841209@gmail.com>
2014-09-24 09:46:44 +0800

    [dev]build

---
+++ app/sf/can.sf.js
@@ -0,0 +1,18 @@
+var sf = sf || {};
+
+sf.Control = can.Control.extend({
+
+
+  /**
+   * @description 主渲染步骤，渲染页面需要被
+   * @return {[type]} [description]
+   */
+  render: function () {
+
+  },
+
+  supplement: function () {
+
+  }
+
+});
